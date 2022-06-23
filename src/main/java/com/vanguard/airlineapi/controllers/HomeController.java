026f6fef3bcdf8da10b91fff6796576712ff6f84
Kelso Griffin <kgriffin.c@xpanxion.com>
2022-06-23 14:35:11 -0500

    Added log4j2 configuration into my application with a runner logger for information

--- src/main/java/com/vanguard/airlineapi/controllers/HomeController.java
+++ src/main/java/com/vanguard/airlineapi/controllers/HomeController.java
@@ -3,25 +3,38 @@ package com.vanguard.airlineapi.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.vanguard.airlineapi.model.Flight;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class HomeController {
+    // Creating a logger
+    private static Logger LOGGER = LogManager.getLogger();
 
     @RequestMapping(value = "/flights", method = RequestMethod.GET)
     public List <Flight> getFlights(Model model) {
         // Requesting all Flights
         List <Flight> flightList = new ArrayList<Flight>();
-        flightList.add(new Flight ("1728", "Dallas, TX", "Detroit, MI"));
-        flightList.add(new Flight ("2516", "Fort Lauderdale, FL", "Los Angeles, CA"));
 
+        Flight f1 = new Flight ("1728", "Dallas, TX", "Detroit, MI");
+        Flight f2 = new Flight ("2516", "Fort Lauderdale, FL", "Los Angeles, CA");
+
+        flightList.add(f1);
+        LOGGER.info("Added to the Flight List - Flight number: " + f1.getFlightNumber() + ", origin: " + f1.getOrigin() + ", destination: " + f1.getDestination());
+        flightList.add(f2);
+        LOGGER.info("Added to the Flight List - Flight number: " + f2.getFlightNumber() + ", origin: " + f2.getOrigin() + ", destination: " + f2.getDestination());
+        LOGGER.trace("Flight List contains : " + flightList.size() + " flights in the system.");
+        LOGGER.error("" +flightList.size());
+        LOGGER.warn("" + flightList.size());
         return flightList;
     }
 }
